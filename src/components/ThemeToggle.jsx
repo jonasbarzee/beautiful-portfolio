f5899e53557dfb3e254a619611d6fc6f70b2afa3
jonasbarzee <jonascbarzee@gmail.com>
2025-11-22 14:18:20 -0700

    Moved the dark and light theme toggle to the top right corner.

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,8 +1,20 @@
 import { Moon, Sun } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {cn} from "@/lib/utils";
 
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme");
+        if (storedTheme == "dark") {
+            document.documentElement.classList.add("dark");
+            setIsDarkMode(true)
+        } else {
+            localStorage.setItem("theme", "light");
+            setIsDarkMode(false);
+        }
+    }, []);
 
     const toggleTheme = () => {
         if (isDarkMode) {
@@ -17,7 +29,7 @@ export const ThemeToggle = () => {
     };
 
     return (
-        <button onClick={toggleTheme}>
+        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300")}>
             {" "}
             {isDarkMode ? (
                 <Sun className="h-6 w-6 text-yellow-300" />
