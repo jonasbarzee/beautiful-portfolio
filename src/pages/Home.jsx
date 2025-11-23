503d90028187d6296ccf473a745faf0e5f14fac9
jonasbarzee <jonascbarzee@gmail.com>
2025-11-23 12:11:31 -0700

    Add mobile and desktop navigation bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,21 @@
-import { ThemeToggle } from "../components/ThemeToggle";
+import { Navbar } from "@/components/Navbar";
+import { ThemeToggle } from "@/components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 
 export const Home = () => {
     return (
-    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-        {/* Theme Toggle */}
-        <ThemeToggle/>
-        {/* Background Effects */}
-        <StarBackground />
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            {/* Theme Toggle */}
+            <ThemeToggle />
+            {/* Background Effects */}
+            <StarBackground />
+            {/* Navbar */}
+            <Navbar />
 
-        {/* Navbar */}
-
-        {/* Main Content */}
+            {/* Main Content */}
 
 
-        {/* Footer */}
-    </div>
+            {/* Footer */}
+        </div>
     );
 };
