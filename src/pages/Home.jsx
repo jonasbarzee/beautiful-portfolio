d1d8487b3210867bfd319b6b0e59bb25d3642865
jonasbarzee <jonascbarzee@gmail.com>
2025-11-24 14:54:37 -0700

    Add projects in project section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
 import { SkillsSection } from "@/components/SkillsSection";
+import { ProjectsSection } from "@/components/ProjectsSection";
 
 export const Home = () => {
     return (
@@ -20,7 +21,7 @@ export const Home = () => {
                 <HeroSection />
                 <AboutSection />
                 <SkillsSection />
-
+                <ProjectsSection />
             </main>
 
 
