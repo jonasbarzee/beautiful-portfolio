a15e0ff9df5922fb66a115a8e4d2da56849ce5ac
jonasbarzee <jonascbarzee@gmail.com>
2025-11-24 17:11:53 -0700

    Fix typo in contact section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -64,7 +64,7 @@ export const ContactSection = () => {
                                     <MapPin className="h-6 w-6 text-primary" />{" "}
                                 </div>
                                 <div>
-                                    <h4 className="font-medium"> Email</h4>
+                                    <h4 className="font-medium"> Location</h4>
                                     <a className="text-muted-foreground hover:text-primary transition-colors">
                                         Provo, Utah, United States
                                     </a>
